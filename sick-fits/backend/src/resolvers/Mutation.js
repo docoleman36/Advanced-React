7b305d1f09d339979e6bb364744760b5e0193feb
Donald Coleman <donaldcoleman@Donalds-MacBook-Pro-2.local>
2019-12-29 11:59:12 -0800

    Adding files

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,14 +8,28 @@ const Mutations = {
         }, info); 
 
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        //first take a copy of the updates
+        const updates = { ...args };
+        //remove the id from the updates
+        delete updates.id;
+        //run the update method 
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id,
+                },
+            },
+            info
+        );
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        const item = await ctx.db.query.item({ where }, `{ id title}`);
+        return ctx.db.mutation.deleteItem({ where }, info);
     }
-
-    // createDog(parents, args, ctx, info) {
-    //     global.dogs = global.dogs || [];
-    //     const newDog = { name: args.name }
-    //     global.dogs.push(newDog);
-    //     return newDog;
-    // }
 };
 
 module.exports = Mutations;
